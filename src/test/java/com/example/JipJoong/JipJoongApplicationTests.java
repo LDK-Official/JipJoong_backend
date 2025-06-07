13c2493a9ee216b8962172cdec2eb7760d417836
Logan <jjk1491@gmail.com>
2025-06-07 18:29:01 +0900

    first commit

---
+++ src/test/java/com/example/JipJoong/JipJoongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JipJoong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JipJoongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
